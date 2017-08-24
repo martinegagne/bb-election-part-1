551f9e0b1061aa1c70937dfce2e912c477fe67c8
Martine Gagne <martine.e.gagne@gmail.com>
2017-08-23 22:16:44 -0400

    complete part 1

--- election.js
+++ election.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-
   // Imagination!
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done(function(responseData) {
+    var candidateArray = responseData.candidates
+    var ul = document.getElementById('candidate_data')
+
+    for (var i = 0; i < candidateArray.length; i++) {
+      var name = candidateArray[i].name;
+      var votes = candidateArray[i].votes;
+      var li = document.createElement('li');
+      li.innerText = name +': '+ votes;
+      ul.append(li);
+      console.log(name, " ", votes);
+    }
+  });
 
 });
